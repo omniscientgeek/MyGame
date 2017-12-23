ac4fa3d39b34528e41440699d127ca03f6f01f24
Justin <justin@omniscientgeek.com>
2017-12-23 15:50:11 -0800

    Rough version with movement from input

--- Assets/Scripts/FooClass.cs
+++ Assets/Scripts/FooClass.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FooClass : MonoBehaviour {
+	private Vector2 touchOrigin = -Vector2.one; //Used to store location of screen touch origin for mobile controls.
+	
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +13,59 @@ public class FooClass : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		int horizontal = 0;     //Used to store the horizontal move direction.
+		int vertical = 0;       //Used to store the vertical move direction.
+		var cube = GameObject.Find ("Cube") as GameObject;
+
+
+		//Check if Input has registered more than zero touches
+		if (Input.touchCount > 0) {
+			//Store the first touch detected.
+			Touch myTouch = Input.touches [0];
+                
+			//Check if the phase of that touch equals Began
+			if (myTouch.phase == TouchPhase.Began) {
+				//If so, set touchOrigin to the position of that touch
+				touchOrigin = myTouch.position;
+			}
+                
+                //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+			else if ((myTouch.phase == TouchPhase.Moved || myTouch.phase == TouchPhase.Stationary)) {
+				//Set touchEnd to equal the position of this touch
+				Vector2 touchEnd = myTouch.position;
+                    
+				//Calculate the difference between the beginning and end of the touch on the x axis.
+				float x = touchEnd.x - touchOrigin.x;
+                    
+				//Calculate the difference between the beginning and end of the touch on the y axis.
+				float y = touchEnd.y - touchOrigin.y;
+                    
+				//Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+				//touchOrigin.x = -1;
+                    
+				//Check if the difference along the x axis is greater than the difference along the y axis.
+				if (Mathf.Abs (x) > Mathf.Abs (y))
+                        //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                        horizontal = x > 0 ? 1 : -1;
+				else
+                        //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                        vertical = y > 0 ? 1 : -1;
+			} else {
+				int foo = 0;
+			}
+		}
+            
+		//Check if we have a non-zero value for horizontal or vertical
+		if (horizontal != 0 || vertical != 0) {
+			//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+			//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+			if (vertical != 0) {
+				cube.transform.Translate (Vector3.back * vertical * Time.deltaTime);
+			}
+			if (horizontal != 0) {
+				cube.transform.Translate (Vector3.left * horizontal * Time.deltaTime);
+			}
+			//AttemptMove<Wall> (horizontal, vertical);
+		}
 	}
 }
